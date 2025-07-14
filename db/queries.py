eae90ac42b48931dc41e01f00b40ec3ef28d3380
Sergii Nosachenko <sergii.n@mate.academy>
2025-07-14 13:37:35 +0300

    Update queries.py

--- db/queries.py
+++ db/queries.py
@@ -1,46 +1,68 @@
 from db.models import Message, User, Chat
 from django.db.models import Q, Count, F
 
-# Code
+
 def get_messages_that_contain_word(word: str) -> list[Message]:
-    pass
+    return list(Message.objects.filter(text__icontains=word))
 
 
 def get_untitled_chats() -> list[Chat]:
-    pass
+    return list(Chat.objects.filter(title__startswith="Untitled"))
 
 
 def get_users_who_sent_messages_in_2015() -> list[str]:
-    pass
+    return list(Message.objects.filter(sent__year=2015).
+                values_list("user__first_name", "user__last_name"))
 
 
 def get_actual_chats() -> list[Chat]:
-    pass
+    return list(Chat.objects.filter(message__sent__year__gt=2020).
+                distinct())
 
 
 def get_messages_contain_authors_first_name() -> list[Message]:
-    pass
+    messages = Message.objects.filter(text__icontains=F("user__first_name"))
+    return list(messages)
 
 
 def get_users_who_sent_messages_starts_with_m_or_a() -> list[User]:
-    pass
+    users = User.objects.filter(
+        Q(message__text__istartswith="a")
+        | Q(message__text__istartswith="m")
+    )
+    return list(users)
 
 
 def get_delivered_or_admin_messages() -> list[Message]:
-    pass
+    return list(Message.objects.filter(
+        Q(user__username__startswith="admin")
+        | Q(is_delivered=True))
+    )
 
 
 def get_count_messages_sent_by_first_name(first_name: str) -> int:
-    pass
+    return (Message.objects.filter(user__first_name=first_name).
+            count)
 
 
 def get_top_users_by_number_of_the_messages() -> list[User]:
-    pass
+    return list(User.objects.
+                annotate(num_messages=Count("message")).
+                order_by("-num_messages")[:3])
 
 
 def get_last_5_messages_dicts() -> list[dict]:
-    pass
+    messages = Message.objects.order_by("-sent")[:5].select_related("user")
+    return [{"from": message.user.username,
+             "text": message.text}
+            for message in messages]
 
 
 def get_chat_dicts() -> list[dict]:
-    pass
+    chats = Chat.objects.prefetch_related("users")
+    return [
+        {"id": chat.id,
+         "title": chat.title,
+         "users": [user.username for user in chat.users.all]
+         }
+        for chat in chats]
